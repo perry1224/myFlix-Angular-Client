69a692a29f55535adc37b498e8054d6cd2f21e06
perry1224 <perry.w1224@gmail.com>
2022-09-17 22:14:14 -0400

    updated files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,21 @@ import { MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
 
-  constructor(public dialog: MatDialog) { }
-// This is the function that will open the dialog when the signup button is clicked  
-openUserRegistrationDialog(): void {
+  constructor(public dialog: MatDialog) {}
+  openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
-// Assigning the dialog a width
-    width: '380px'
+      width: '480px',
+    });
+  }
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      width: '480px',
     });
   }
 
-openUserLoginDialog(): void {
-  this.dialog.open(UserLoginFormComponent, {
-    width: '380px',
-  });
-}
-}
+  }
--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,47 +1,50 @@
 import { Component, OnInit, Input } from '@angular/core';
-// You'll use this import to close the dialog on success
 import { MatDialogRef } from '@angular/material/dialog';
-
-// This import brings in the API calls we created in 6.2
 import { FetchApiDataService } from '../fetch-api-data.service';
-
-// This import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-user-login-form',
   templateUrl: './user-login-form.component.html',
   styleUrls: ['./user-login-form.component.scss']
+  
 })
 export class UserLoginFormComponent implements OnInit {
-@Input() userData = { Username: '', Password: ''};
 
-constructor(
-  public fetchApiData: FetchApiDataService,
-  public dialogRef: MatDialogRef<UserLoginFormComponent>,
-  public snackBar: MatSnackBar,
-) { }
+  @Input() userData = { Username: '', Password: '' };
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar,
+    private router : Router) { }
 
   ngOnInit(): void {
   }
 
+  /**
+   * sending form inputs for user login to backend via fetchApiData Service
+   */
   loginUser(): void {
     this.fetchApiData.userLogin(this.userData).subscribe((response) => {
-  // Logic for a successful user login goes here! (To be implemented)
-     this.dialogRef.close(); // This will close the modal on success!
-     console.log(response)
-     // Add token and username to local storage
-     localStorage.setItem('token', response.token);
-     localStorage.setItem('username', response.user.Username)
-     this.snackBar.open('User has logged in', 'OK', {
+      // TBD: Logic for successfull login!
+      this.dialogRef.close(); // Close the modal on success
+      console.log(response);
+      // Add token and username to local Storage
+      localStorage.setItem('token', response.token);
+      localStorage.setItem('username', response.user.Username);
+      console.log(response);
+      this.snackBar.open(response, 'OK', {
         duration: 2000
-     });
+      });
+
     }, (response) => {
-      console.log(response)
-      this.snackBar.open('User has logged in', 'OK', {
+      console.log(response);
+      this.snackBar.open(response, 'OK', {
         duration: 2000
       });
     });
   }
-
-}
+} 
